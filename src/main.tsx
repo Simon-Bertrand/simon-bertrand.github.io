e2ddb4842585a0ece292b70311a88cd0490db3ad
Simon Bertrand <84455908+Simon-Bertrand@users.noreply.github.com>
2023-07-29 22:43:59 +0200

    went to hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './style/index.css'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { NavLink, Routes, Route } from "react-router-dom";
 import { Menus, SocialMedias } from "./params";
 import Pages from "./partials/Page";
