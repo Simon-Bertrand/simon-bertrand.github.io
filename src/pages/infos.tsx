2182f6a33342b68214525e19df99930116438805
SiMB <84455908+Simon-Bertrand@users.noreply.github.com>
2023-08-02 13:18:11 +0200

    Update infos.tsx

--- src/pages/infos.tsx
+++ src/pages/infos.tsx
@@ -8,7 +8,7 @@ export default function InfosPage() {
     return <>
     
 
-        <div className="text-center w-full mx-auto  px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
+        <div className="text-center text-white w-full mx-auto  px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
             <h2 className="text-3xl font-extrabold text-black dark:text-white sm:text-4xl">
                 <span className="block">
                     I'm Simon Bertrand
