0c3488e7c70f20faf8944b44b782ed1da3e8f478
Simon Bertrand <simonbertrand.contact@gmail.com>
2024-07-18 09:29:43 +0200

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,112 +1,123 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './style/index.css'
-import { HashRouter, Outlet, useLocation } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style/index.css";
+import { HashRouter, Outlet, useLocation } from "react-router-dom";
 import { NavLink, Routes, Route } from "react-router-dom";
 import { Menus, SocialMedias } from "./params";
 import Pages from "./partials/Page";
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
 const AnimationLayout = () => {
   const { pathname } = useLocation();
   return (
-      <motion.div
-        key={pathname}
-        initial="initial"
-        animate="in"
-        variants={{
-          initial: {
-            opacity: 0
-          },
-          in: {
-            opacity: 1
-          },
-          out: {
-            opacity:  0
-          },
-        }}
-        transition={{
-          ease: "easeInOut",
-          duration: 0.3
-        }}
-      >
-        <Outlet />
-      </motion.div>
+    <motion.div
+      key={pathname}
+      initial="initial"
+      animate="in"
+      variants={{
+        initial: {
+          opacity: 0,
+        },
+        in: {
+          opacity: 1,
+        },
+        out: {
+          opacity: 0,
+        },
+      }}
+      transition={{
+        ease: "easeInOut",
+        duration: 0.3,
+      }}
+    >
+      <Outlet />
+    </motion.div>
   );
 };
-
 
 function App() {
   return (
     <>
-    <div className='layout'>
-      <div className='p-3'>
-        <div className="bg-color rounded-xl p-3 shadow-xl h-full flex items-center">
-          <div className='w-full'>
-            <img src="avatar.jpg" alt="Simon Bertrand" title="Simon Bertrand Photo" className="avatar" />
-            <h1 className="title">Simon Bertrand</h1>
-            <h5 className="subtitle">Ingénieur doctorant en Computer Vision</h5>
-            <div className="flex text-stone-400 justify-center gap-1 pt-3">
-            {
-              SocialMedias.map(x => {
-                return (
-                  <a href={x.href} target='_blank' title={x.name + " Simon Bertrand"} >{x.svg}</a>
-                )
-              })
-            }
-            </div>
-            <div className="rounded-2xl ">
-                <div className="flex items-center justify-center">
-                </div>
+      <div className="layout">
+        <div className="p-3">
+          <div className="bg-color rounded-xl p-3 shadow-xl h-full flex items-center">
+            <div className="w-full">
+              <img
+                src="avatar.jpg"
+                alt="Simon Bertrand"
+                title="Simon Bertrand Photo"
+                className="avatar"
+              />
+              <h1 className="title">Simon Bertrand</h1>
+              <h5 className="subtitle">Doctorant en deep learning</h5>
+              <div className="flex text-stone-400 justify-center gap-1 pt-3">
+                {SocialMedias.map((x) => {
+                  return (
+                    <a
+                      href={x.href}
+                      target="_blank"
+                      title={x.name + " Simon Bertrand"}
+                    >
+                      {x.svg}
+                    </a>
+                  );
+                })}
+              </div>
+              <div className="rounded-2xl ">
+                <div className="flex items-center justify-center"></div>
                 <nav className="mt-6">
-                    <div>
-                      {
-                        Menus.map((x) =>
-                          <NavLink 
-                          className={({ isActive }) => isActive ? "menu-item-active" : "menu-item"}
-                          to={x.href} >
-                            <span className="text-left">
-                                {x.svg}
-                            </span>
-                            <span className="mx-4 text-sm font-normal">
-                                {x.name}
-                            </span>
-                          </NavLink>
-                        )
-                      }
-                    </div>
+                  <div>
+                    {Menus.map((x) => (
+                      <NavLink
+                        className={({ isActive }) =>
+                          isActive ? "menu-item-active" : "menu-item"
+                        }
+                        to={x.href}
+                      >
+                        <span className="text-left">{x.svg}</span>
+                        <span className="mx-4 text-sm font-normal">
+                          {x.name}
+                        </span>
+                      </NavLink>
+                    ))}
+                  </div>
                 </nav>
+              </div>
             </div>
           </div>
         </div>
-
-
-      </div>
-      <div className='col-span-2 md:overflow-auto p-3'>
+        <div className="col-span-2 md:overflow-auto p-3">
           <Routes>
             <Route element={<AnimationLayout />}>
-              {
-                Menus.map(x => {return <Route path = {x.href} element={<Pages href={x.href}>{x.page}</Pages>} />})
-              }
-              <Route path = "*" element={<Pages href={Menus[0].href}>{Menus[0].page}</Pages>} />
+              {Menus.map((x) => {
+                return (
+                  <Route
+                    path={x.href}
+                    element={<Pages href={x.href}>{x.page}</Pages>}
+                  />
+                );
+              })}
+              <Route
+                path="*"
+                element={<Pages href={Menus[0].href}>{Menus[0].page}</Pages>}
+              />
             </Route>
           </Routes>
+        </div>
       </div>
-    </div>
-    <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center">
         <footer className="text-stone-400">
           © {new Date().getFullYear()} Simon Bertrand. Tous droits réservés.
         </footer>
-    </div>
+      </div>
     </>
-  )
+  );
 }
 
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-      <HashRouter>
-        <App />
-      </HashRouter>
-  </React.StrictMode>,
-)
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
--- src/pages/infos.tsx
+++ src/pages/infos.tsx
@@ -1,122 +1,116 @@
 import { InfosStats } from "../params";
 
-
-
 export default function InfosPage() {
+  return (
+    <>
+      <div className="text-center w-full mx-auto  px-4 sm:px-6 lg:py-10 lg:px-8 z-20">
+        <h2 className="text-3xl font-extrabold sm:text-4xl text-white">
+          <span className="block">Simon Bertrand</span>
+          <span className="color">Ingénieur doctorant en deep learning</span>
+        </h2>
+        <p className="text-xl my-4 max-w-xl mx-auto text-gray-400">
+          <ul>
+            <li>Science des données et IA (ML, DL, CV)</li>
+            <li>Software Engineering (Web, Logiciels)</li>
+          </ul>
+        </p>
+        <section>
+          <div className="container flex justify-around pt-3">
+            {InfosStats.map((x) => (
+              <div>
+                <h5 className="text-5xl font-bold text-white">
+                  <span className="inline text-white">{x.value}</span>
+                  <span className="color">+</span>
+                </h5>
+                <p className="text-xs font-medium tracking-wide text-indigo-100 uppercase">
+                  {x.name}
+                </p>
+              </div>
+            ))}
+          </div>
+        </section>
+      </div>
 
+      <div className="max-w-screen-xl p-3 mx-auto">
+        <h2 className="mb-8 text-3xl font-extrabold border-b-4">
+          Domaines techniques
+        </h2>
 
-    return <>
-    
-
-        <div className="text-center w-full mx-auto  px-4 sm:px-6 lg:py-10 lg:px-8 z-20">
-            <h2 className="text-3xl font-extrabold sm:text-4xl text-white">
-                <span className="block">
-                    Simon Bertrand
-                </span>
-                <span className="color">
-                   Ingénieur doctorant en Computer Vision / IA
-                </span>
-            </h2>
-            <p className="text-xl my-4 max-w-xl mx-auto text-gray-400">
-                Science des données & IA (ML, DL, CV) et Software Engineering (Web, IHM) avec Python, Rust, C#, SQL et ReactJS.
-            </p>
-            <section>
-                <div className="container flex justify-around pt-3">
-                    {
-                        InfosStats.map(x => 
-                            <div>
-                                <h5 className="text-5xl font-bold text-white">
-                                    <span className="inline text-white">
-                                        {x.value}
-                                    </span>
-                                    <span className="color">
-                                        +
-                                    </span>
-                                </h5>
-                                <p className="text-xs font-medium tracking-wide text-indigo-100 uppercase">
-                                    {x.name}
-                                </p>
-                            </div>
-                        )
-                    }
-                </div>
-            </section>
-        </div>
-        
-        <div className="max-w-screen-xl p-3 mx-auto">
-            <h2 className="mb-8 text-3xl font-extrabold border-b-4">
-                Domaines techniques
-            </h2>
-            
-            <div className="grid grid-cols-2 text-center items-center gap-8">
-                <div className="p-4 overflow-hidden bg-color2 shadow-lg rounded-2xl">
-                        <div className="w-full">
-                            <p className="mb-2 text-md lg:text-lg font-medium text-white">
-                                IA / Machine Learning 
-                            </p>
-                            <p className="text-xs text-gray-400">
-                                TensorFlow, Keras, PyTorch, Jax, Sci-kit Learn, Ray
-                            </p>
-                        </div>
-                </div>
-                <div className="p-4 overflow-hidden bg-color2 shadow-lg rounded-2xl">
-                    <div className="w-full">
-                        <p className="mb-2 text-md lg:text-lg font-medium text-white">
-                        Traitement du signal et des images
-                        </p>
-                        <p className="text-xs text-gray-400">
-                        OpenCV, SciPy, Numpy, Pandas, Statsmodels, MATLAB Toolbox
-                        </p>
-                    </div>
-                </div>
-                <div className="p-4 overflow-hidden bg-color2 shadow-lg rounded-2xl">
-                    <div className="w-full">
-                        <p className="mb-2 text-md lg:text-lg font-medium text-white">
-                            Data Engineering
-                        </p>
-                        <p className="text-xs text-gray-400">
-                            SQL, No-SQL, T-SQL, PL/pgSQL, PySpark, ORM, ETL
-                        </p>
-                    </div>
-                </div>
-
-                <div className="p-4 overflow-hidden bg-color2 shadow-lg rounded-2xl">
-                    <div className="w-full">
-                        <p className="mb-2 text-md lg:text-lg font-medium text-white">
-                            Développement web
-                        </p>
-                        <p className="text-xs text-gray-400">
-                            HTML, CSS, JavaScript, C# (ASP .NET), Node.js, TypeScript, ReactJS, Next.js, Tailwind CSS, Bootstrap
-                        </p>
-                    </div>
-                </div>
-                <div className="p-4 overflow-hidden bg-color2 shadow-lg rounded-2xl">
-                    <div className="w-full">
-                        <p className="mb-2 text-md lg:text-lg font-medium text-white">
-                            Programmation bas niveau
-                        </p>
-                        <p className="text-xs text-gray-400">
-                            Rust, C/C++ 
-                        </p>
-                    </div>
-                </div>
-                <div className="p-4 overflow-hidden bg-color2 shadow-lg rounded-2xl">
-                    <div className="w-full">
-                        <p className="mb-2 text-md lg:text-lg font-medium text-white">
-                        Réseau et administration système
-                        </p>
-                        <p className="text-xs text-gray-400">
-                        Windows, Linux, Ubuntu Server, Serveurs HTTP, SSL, Docker, SSH. <br />
-                            Notions en réseau
-                        </p>
-                    </div>
-                </div>
+        <div className="grid grid-cols-2 text-center items-center gap-8">
+          <div className="p-4 overflow-hidden bg-color2 shadow-lg rounded-2xl">
+            <div className="w-full">
+              <p className="mb-2 text-md lg:text-lg font-medium text-white">
+                IA / Machine Learning
+              </p>
+              <p className="text-xs text-gray-400">
+                TensorFlow, Keras, PyTorch, Jax, Sci-kit Learn, Ray
+              </p>
             </div>
+          </div>
+          <div className="p-4 overflow-hidden bg-color2 shadow-lg rounded-2xl">
+            <div className="w-full">
+              <p className="mb-2 text-md lg:text-lg font-medium text-white">
+                Traitement du signal et des images
+              </p>
+              <p className="text-xs text-gray-400">
+                OpenCV, SciPy, Numpy, Pandas, Statsmodels, MATLAB, Google Earth
+                Engine
+              </p>
+            </div>
+          </div>
+          <div className="p-4 overflow-hidden bg-color2 shadow-lg rounded-2xl">
+            <div className="w-full">
+              <p className="mb-2 text-md lg:text-lg font-medium text-white">
+                Data Engineering
+              </p>
+              <p className="text-xs text-gray-400">
+                SQL, No-SQL, Redis Cache, T-SQL, PL/pgSQL, PySpark, ORM, ETL.
+                <br />
+                <p className="text-[10px] italic">
+                  {" "}
+                  Bonne maîtrise de PostgreSQL et de ces fonctionnalités
+                  avancées : notify/listen, procédures stockées, triggers,
+                  extension TimescaleDB...
+                </p>
+              </p>
+            </div>
+          </div>
 
-          
+          <div className="p-4 overflow-hidden bg-color2 shadow-lg rounded-2xl">
+            <div className="w-full">
+              <p className="mb-2 text-md lg:text-lg font-medium text-white">
+                Développement web
+              </p>
+              <p className="text-xs text-gray-400">
+                HTML, CSS, JavaScript, C# (ASP .NET), Node.js, TypeScript,
+                ReactJS, Next.js, Tailwind CSS, Bootstrap
+              </p>
+            </div>
+          </div>
+          <div className="p-4 overflow-hidden bg-color2 shadow-lg rounded-2xl">
+            <div className="w-full">
+              <p className="mb-2 text-md lg:text-lg font-medium text-white">
+                Programmation bas niveau
+              </p>
+              <p className="text-xs text-gray-400">Rust, C/C++</p>
+            </div>
+          </div>
+          <div className="p-4 overflow-hidden bg-color2 shadow-lg rounded-2xl">
+            <div className="w-full">
+              <p className="mb-2 text-md lg:text-lg font-medium text-white">
+                Réseau et administration système
+              </p>
+              <p className="text-xs text-gray-400">
+                Ubuntu Server, serveurs web (protocole HTTP, WS, ...), Docker,
+                CI/CD, IP, Proxy, DNS, NAT, SSL, Port Forwarding, ... <br />
+                <p className="text-[10px] italic">
+                  Utilisation de supercalculateurs (HPC) pour le calcul intensif
+                </p>
+              </p>
+            </div>
+          </div>
         </div>
-
-
-
+      </div>
     </>
+  );
 }
