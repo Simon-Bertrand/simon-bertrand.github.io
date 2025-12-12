7ade04a9e2fae61dd13f1c5e39510810df817e0b
Simon-Bertrand <simonbertrand.contact@gmail.com>
2025-12-13 00:18:35 +0100

    small ui fix

--- src/pages/education.tsx
+++ src/pages/education.tsx
@@ -2,22 +2,24 @@ import { EducationItems } from "../params"
 
 function EducationEntity({title, description, date, option, src} : {title : string, description : string, date: string, option : string, src:string }) {
     return (
-        <div className="flex max-w-md overflow-hidden bg-color2 rounded-lg shadow-lg">
-            <div className="w-full p-4 text-center my-auto">
-                <h1 className="text-lg font-bold text-white">
-                    {title}
-                </h1>
-                <h2 className="text-xs italic text-gray-400">
-                    Option : {option}
-                </h2>
-                <p className="mt-2 text-sm text-gray-400">
-                    {description}
-                </p>
-                <img className="w-1/2 mx-auto m-3 object-contain grayscale brightness-120	" src={"logos/"+src} />
-                <div className="flex justify-center mt-3 item-center">
-                    <p className="text-xl text-center text-gray-200">
-                        {date}
+        <div className="flex justify-center w-full">
+            <div className="flex flex-col max-w-md w-full overflow-hidden bg-color2 rounded-lg shadow-lg m-2">
+                <div className="w-full p-4 text-center m-auto">
+                    <h1 className="text-lg font-bold text-white">
+                        {title}
+                    </h1>
+                    <h2 className="text-xs italic text-gray-400">
+                        Option : {option}
+                    </h2>
+                    <p className="mt-2 text-sm text-gray-400">
+                        {description}
                     </p>
+                    <img className="w-1/2 mx-auto m-3 object-contain grayscale brightness-120" src={"logos/" + src} />
+                    <div className="flex justify-center mt-3 items-center">
+                        <p className="text-xl text-center text-gray-200">
+                            {date}
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
